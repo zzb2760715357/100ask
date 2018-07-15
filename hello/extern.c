dc6717bb113af1573fb0dbc69a6f6e3ee0cba443
zhuangzebin <2760715357@qq.com>
2018-07-15 16:50:52 +0800

    Makefile Simple Demo

---
+++ hello/extern.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void hi_fun()
+{
+    printf("Hi zhuangzb\r\n");
+}
---
+++ hello/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+extern void hi_fun();
+
+int main()
+{
+    printf("hello world\r\n");
+    hi_fun();
+    
+    return 0;
+}
