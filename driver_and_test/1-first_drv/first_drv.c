220470b5a52910f840adc0eae963b7422a237ce3
zhuangzb <2760715357@qq.com>
2018-08-15 16:37:48 +0800

    add led driver test

--- driver_and_test/1-first_drv/first_drv.c
+++ driver_and_test/1-first_drv/first_drv.c
@@ -17,9 +17,15 @@ static int major;
 static struct class *firstdrv_class;
 static struct class_device *firstdrv_class_dev;
 
+volatile unsigned long *gpfcon = NULL;
+volatile unsigned long *gpfdat = NULL;
+
 static int first_drv_open (struct inode *inode, struct file *file)
 {
+	/*ÅäÖÃgpioÎªÊä³ö×´Ì¬*/
 	printk("--- %s --- \r\n",__func__);
+	*gpfcon &= ~((0x3<<(4*2))|(0x3<<(5*2))|(0x3<<(6*2))); 
+	*gpfcon |= ((0x1<<(4*2))|(0x1<<(5*2))|(0x1<<(6*2)));
 
 	return 0;
 }
@@ -27,7 +33,23 @@ static int first_drv_open (struct inode *inode, struct file *file)
 
 static ssize_t first_drv_write (struct file *file, const char __user *buf, size_t size, loff_t *ppos)
 {
+	int val;
+	
 	printk("--- %s --- \r\n",__func__);
+	if (copy_from_user(&val,buf,size))
+		return -EFAULT;
+
+	printk("kernel:val = %d\r\n",val);
+
+	if (val == 1){
+		//µãµÆ
+		printk("--- %s:%d ---\r\n",__func__,__LINE__);
+		*gpfdat &= ~((1<<4)|(1<<5)|(1<<6));
+	}else{
+		//ÃğµÆ
+		printk("--- %s:%d ---\r\n",__func__,__LINE__);
+		*gpfdat |= ((1<<4)|(1<<5)|(1<<6));
+	}
 
 	return 0;
 }
@@ -50,6 +72,9 @@ static int __init first_drv_init(void)
 
 	firstdrv_class_dev = class_device_create(firstdrv_class,NULL,MKDEV(major,0),NULL,"xxx");
 
+	gpfcon =(volatile unsigned long *) ioremap(0x56000050,16);
+	gpfdat = gpfcon + 1;
+
 	return 0;
 }
 
@@ -59,6 +84,7 @@ static void __exit first_drv_exit(void)
 	unregister_chrdev(111,"first_drv");
 	class_device_unregister(firstdrv_class_dev);
 	class_destroy(firstdrv_class);
+	iounmap(gpfcon);
 
 }
 
--- driver_and_test/1-first_drv/first_test.c
+++ driver_and_test/1-first_drv/first_test.c
@@ -3,16 +3,33 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+
+
 int main(int argc,char **argv)
 {
 	int fd = -1;
-	int val = 1;
+	int val = -1;
+
+	if (argc != 2){
+		printf("usage:\r\n");
+		printf("%s <on|off>\r\n",argv[0]);
+		return 0;
+	}
 
 	fd = open("/dev/xxx",O_RDWR);
 	if (fd < 0){
 		printf("open /dev/xxx error \r\n");
 		return -1;
 	}
+
+	if (strcmp(argv[1],"on") == 0){
+		val = 1;
+	}else  {
+		val = 0;
+	}
+	
+	fprintf(stderr,"APP:val = %d\r\n",val);
+	
 	write(fd,&val,4);
 
 	return 0;	
