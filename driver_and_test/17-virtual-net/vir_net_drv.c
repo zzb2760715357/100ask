9ca5935288edc3e1851a132167be4cfc23214274
zhuangzb <2760715357@qq.com>
2018-08-27 16:36:44 +0800

    add send packet

--- driver_and_test/17-virtual-net/vir_net_drv.c
+++ driver_and_test/17-virtual-net/vir_net_drv.c
@@ -24,7 +24,14 @@
 
 static struct net_device *vir_net_dev; 
 
+static	int	virtual_net_send_packet(struct sk_buff *skb,struct net_device *dev)
+{
+	static int cnt = 0;
 
+	printk("virtual_net_send_packet cnt = %d\r\n",++cnt);
+
+	return 0;
+}
 
 static int __init virtual_net_init(void)
 {
@@ -32,6 +39,7 @@ static int __init virtual_net_init(void)
 
 	vir_net_dev = alloc_netdev(0, "vnet%d", ether_setup);
 
+	vir_net_dev->hard_start_xmit 	= virtual_net_send_packet;
 
 	register_netdev(vir_net_dev);
 
@@ -44,7 +52,6 @@ static void __exit virtual_net_exit(void)
 
 	unregister_netdev(vir_net_dev);
 	free_netdev(vir_net_dev);
-	
 }
 
 
