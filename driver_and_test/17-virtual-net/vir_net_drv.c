cbddaa629bf929e14f6907a302ec40a7f64db0f9
zhuangzb <2760715357@qq.com>
2018-08-27 16:52:57 +0800

    add emulator rx packet

--- driver_and_test/17-virtual-net/vir_net_drv.c
+++ driver_and_test/17-virtual-net/vir_net_drv.c
@@ -20,15 +20,78 @@
 #include <asm/io.h>
 #include <asm/irq.h>
 #include <asm/dma.h>
+#include <linux/ip.h>
 
 
 static struct net_device *vir_net_dev; 
+
+static void emulator_rx_packet(struct sk_buff *skb, struct net_device *dev)
+{
+	/* 参考LDD3 */
+	unsigned char *type;
+	struct iphdr *ih;
+	__be32 *saddr, *daddr, tmp;
+	unsigned char	tmp_dev_addr[ETH_ALEN];
+	struct ethhdr *ethhdr;
+	
+	struct sk_buff *rx_skb;
+		
+	// 从硬件读出/保存数据
+	/* 对调"源/目的"的mac地址 */
+	ethhdr = (struct ethhdr *)skb->data;
+	memcpy(tmp_dev_addr, ethhdr->h_dest, ETH_ALEN);
+	memcpy(ethhdr->h_dest, ethhdr->h_source, ETH_ALEN);
+	memcpy(ethhdr->h_source, tmp_dev_addr, ETH_ALEN);
+
+	/* 对调"源/目的"的ip地址 */    
+	ih = (struct iphdr *)(skb->data + sizeof(struct ethhdr));
+	saddr = &ih->saddr;
+	daddr = &ih->daddr;
+
+	tmp = *saddr;
+	*saddr = *daddr;
+	*daddr = tmp;
+	
+	//((u8 *)saddr)[2] ^= 1; /* change the third octet (class C) */
+	//((u8 *)daddr)[2] ^= 1;
+	type = skb->data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+	//printk("tx package type = %02x\n", *type);
+	// 修改类型, 原来0x8表示ping
+	*type = 0; /* 0表示reply */
+	
+	ih->check = 0;		   /* and rebuild the checksum (ip needs it) */
+	ih->check = ip_fast_csum((unsigned char *)ih,ih->ihl);
+	
+	// 构造一个sk_buff
+	rx_skb = dev_alloc_skb(skb->len + 2);
+	skb_reserve(rx_skb, 2); /* align IP on 16B boundary */	
+	memcpy(skb_put(rx_skb, skb->len), skb->data, skb->len);
+
+	/* Write metadata, and then pass to the receive level */
+	rx_skb->dev = dev;
+	rx_skb->protocol = eth_type_trans(rx_skb, dev);
+	rx_skb->ip_summed = CHECKSUM_UNNECESSARY; /* don't check it */
+	dev->stats.rx_packets++;
+	dev->stats.rx_bytes += skb->len;
+
+	// 提交sk_buff
+	netif_rx(rx_skb);
+}
+
 
 static	int	virtual_net_send_packet(struct sk_buff *skb,struct net_device *dev)
 {
 	static int cnt = 0;
 
 	printk("virtual_net_send_packet cnt = %d\r\n",++cnt);
+
+	netif_stop_queue(dev);
+
+	emulator_rx_packet(skb,dev);
+
+	dev_kfree_skb(skb);
+	netif_wake_queue(dev);
+	
 
 
 	//更新统计信息
@@ -53,6 +116,9 @@ static int __init virtual_net_init(void)
     vir_net_dev->dev_addr[3] = 0x89;
     vir_net_dev->dev_addr[4] = 0x89;
     vir_net_dev->dev_addr[5] = 0x89;
+
+	vir_net_dev->flags           |= IFF_NOARP;
+	vir_net_dev->features        |= NETIF_F_NO_CSUM;	
 
 	register_netdev(vir_net_dev);
 
