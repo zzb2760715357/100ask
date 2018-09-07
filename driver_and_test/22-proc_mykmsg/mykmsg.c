e269f93391ef31d660815cfcf98810a3e0d73a8a
zhuangzb <2760715357@qq.com>
2018-09-07 23:35:54 +0800

    cat output all message when open /proc/mykmsg

--- driver_and_test/22-proc_mykmsg/mykmsg.c
+++ driver_and_test/22-proc_mykmsg/mykmsg.c
@@ -22,6 +22,7 @@ static struct proc_dir_entry *entry;
 static char mylog_buf[MYLOG_BUF_LEN];
 static char tmp_buf[MYLOG_BUF_LEN];
 static int  mylog_r = 0;
+static int  mylog_r_for_read = 0;
 static int  mylog_w = 0;
 
 static DECLARE_WAIT_QUEUE_HEAD(mymsg_waitq);
@@ -31,6 +32,12 @@ static int is_mylog_empty(void)
 {
 	return (mylog_r == mylog_w);
 }
+
+static int is_mylog_empty_for_read(void)
+{
+	return (mylog_r_for_read == mylog_w);
+}
+
 
 static int is_mylog_full(void)
 {
@@ -46,6 +53,9 @@ static void mylog_putc(char c)
 	if (is_mylog_full()){
 		//¶ªµôÒ»¸öÊî¼Ù
 		mylog_r = (mylog_r + 1) % MYLOG_BUF_LEN;
+
+		if ((mylog_r_for_read + 1)%MYLOG_BUF_LEN == mylog_r)
+			mylog_r_for_read = mylog_r;
 	}
 	
 	mylog_buf[mylog_w] = c;
@@ -64,6 +74,18 @@ static int mylog_getc(char *p)
 
 	return 1;
 }
+
+static int mylog_getc_for_read(char *p)
+{
+	if (is_mylog_empty_for_read()){
+		return 0;
+	}
+	*p = mylog_buf[mylog_r_for_read];
+	mylog_r_for_read = (mylog_r_for_read + 1) % MYLOG_BUF_LEN;
+
+	return 1;
+}
+
 
 int myprintk(const char *fmt, ...)
 {
@@ -90,12 +112,12 @@ ssize_t mykmsg_read (struct file *file, char __user *buf, size_t count, loff_t *
 	char c;
 	
 
-	if ((file->f_flags & O_NONBLOCK) && is_mylog_empty())
+	if ((file->f_flags & O_NONBLOCK) && is_mylog_empty_for_read())
 		return -EAGAIN;
 	
-	error = wait_event_interruptible(mymsg_waitq,!is_mylog_empty());
+	error = wait_event_interruptible(mymsg_waitq,!is_mylog_empty_for_read());
 	
-	while (!error && (mylog_getc(&c)) && i < count) {
+	while (!error && (mylog_getc_for_read(&c)) && i < count) {
 			error = __put_user(c,buf);
 			buf++;
 			i++;
@@ -108,7 +130,16 @@ ssize_t mykmsg_read (struct file *file, char __user *buf, size_t count, loff_t *
 
 }
 
+static int mykmsg_open (struct inode *inode, struct file *file)
+{
+	mylog_r_for_read = mylog_r;
+	
+	return 0;
+}
+
+
 static const struct file_operations proc_fops = {
+	.open = mykmsg_open,
 	.read = mykmsg_read,
 };
 
