dcde45c128264bd71752989fcd6cee1785fcfe59
zhuangzb <2760715357@qq.com>
2018-09-12 00:33:57 +0800

    fix input emulaters bugs

---
+++ driver_and_test/27-debug-input-emulate/input_replay.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#define INPUT_REPLAY 0
+#define INPUT_TAG    1
+
+
+void print_usage(char *file)
+{
+	printf("Usage:\r\n");
+	printf("%s write <file>\r\n",file);
+	printf("%s replay \r\n",file);
+	printf("%s tag <string>\r\n",file);
+}
+
+int main(int argc,char **argv)
+{
+	int fd;
+	int fd_data;
+	int len;
+	int buf[100];
+
+	if (argc != 2 && argc != 3){
+		print_usage(argv[0]);
+		return -1;
+	}
+
+	fd = open("/dev/input_emu",O_RDWR);
+	if (fd < 0){
+		printf("can't open /dev/input_emu\r\n");
+		return -1;
+	}
+
+	if (strcmp(argv[1],"replay") == 0){
+		ioctl(fd,INPUT_REPLAY);
+	}else if (strcmp(argv[1],"write") == 0){
+		if (argc != 3){
+			print_usage(argv[0]);
+			return -1;
+		}
+
+		fd_data = open(argv[2],O_RDONLY);
+		if (fd_data < 0){
+			printf("can't open %s\r\n",argv[2]);
+			return -1;
+		}
+
+		while(1){
+			len = read(fd_data,buf,100);
+			if (len == 0){
+				printf("write ok\r\n");
+				break;
+			}else{
+				write(fd_data,buf,len);
+			}
+		}
+	}else if(strcmp(argv[1],"tag") == 0){
+		if (argc != 3){
+			print_usage(argv[0]);
+			return -1;
+		}
+		ioctl(fd,INPUT_TAG,argv[2]);
+	}else{
+		print_usage(argv[0]);
+		return -1;
+	}
+
+	return 0;
+}
+
--- driver_and_test/27-debug-input-emulate/s3c_ts.c
+++ driver_and_test/27-debug-input-emulate/s3c_ts.c
@@ -10,6 +10,8 @@
 #include <linux/clk.h>
 #include <asm/io.h>
 #include <asm/irq.h>
+#include <asm/uaccess.h>
+
 
 #include <asm/plat-s3c24xx/ts.h>
 
@@ -32,7 +34,159 @@ struct s3c_ts_regs{
 static volatile struct s3c_ts_regs *s3c_ts_regs;
 static struct timer_list ts_timer;
 
+#define MYLOG_BUF_LEN (1024*1024)
+#define INPUT_REPLAY 0
+#define INPUT_TAG    1
+
+static int major = 0;
+static struct class *cls;
+static struct timer_list replay_timer;
+static char *replay_buf;
+static int replay_r = 0;
+static int replay_w = 0;
+
 extern int myprintk(const char *fmt, ...);
+
+static ssize_t replay_write (struct file *file, const char __user *buf, size_t count, loff_t *offset)
+{
+	int err;
+
+	if (replay_w + count >= MYLOG_BUF_LEN){
+		printk("replay_buf full!\r\n");\
+		return -EIO;
+	}
+
+	err = copy_from_user(replay_buf + replay_w, buf, count);
+	if (err){
+		return -EIO;
+	}else{
+		replay_w += count;
+	}
+
+	return count;
+	
+}
+
+static int replay_ioctl (struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg)
+{
+	char buf[100];
+
+	switch(cmd){
+		case INPUT_REPLAY:
+		{
+			replay_timer.expires = jiffies + 1;
+			printk("replay_ioctl add timer\r\n");
+			add_timer(&replay_timer);
+			break;
+		}
+		case INPUT_TAG:
+		{
+			if(copy_from_user(buf, (const void __user *)arg, 100))
+				return -EFAULT;
+
+			buf[99] = '\0';
+			myprintk("%s\r\n",buf);
+			break;
+		}
+	}
+
+	return 0;
+	
+}
+
+static int replay_get_line(char *line)
+{
+	int i = 0;
+
+	while(replay_r <= replay_w){
+		if ((replay_buf[replay_r] == ' ')||(replay_buf[replay_r] == '\n') ||(replay_buf[replay_r] == '\t')||(replay_buf[replay_r] == '\t'))
+			replay_r ++;
+		else
+			break;
+	}
+
+	while(replay_r <= replay_w){
+		if ((replay_buf[replay_r] == '\n')||(replay_buf[replay_r] == '\r'))
+			break;
+		else{
+			line[i] = replay_buf[replay_r];
+			replay_r ++;
+			i ++;
+		}
+	}
+
+	line[i] = '\0';
+
+	return i;
+	
+}
+
+static void input_replay_timer_functions(unsigned long data)
+{
+	unsigned int time;
+	unsigned int type;
+	unsigned int code;
+	int val;
+
+	static unsigned int pre_time = 0,pre_type = 0,pre_code = 0;
+	static int pre_val = 0;
+
+	char line[100];
+	int ret;
+
+	if (pre_time !=0){
+		input_event(s3c_ts_dev,pre_type,pre_code,pre_val);
+	}
+
+	while(1){
+		ret = replay_get_line(line);
+		if (ret == 0){
+			printk("end of input replay\r\n");
+			del_timer(&replay_timer);
+			pre_time = pre_type = pre_code = 0;
+			pre_val  = 0;
+			replay_r = replay_w = 0;
+			break;
+		}
+
+		time = 0;
+		type = 0;
+		code = 0;
+		val  = 0;
+		sscanf(line,"%x %x %x %d",&time,&type,&code,&val);
+
+		if (!time && !type && !code && !val)
+			continue;
+		else{
+			if ((pre_time == 0)||(time == pre_time)){
+				input_event(s3c_ts_dev,type,code,val);
+
+				if (pre_time == 0)
+					pre_time = time;
+			}else {
+				mod_timer(&replay_timer,jiffies+(time-pre_time));
+
+				pre_time = time;
+				pre_type = type;
+				pre_code = code;
+				pre_val  = val;
+
+				break;
+			}
+		}
+	}
+
+	
+	
+
+}
+
+static const struct file_operations replay_fops = {
+	.owner = THIS_MODULE,
+	.write = replay_write,
+	.ioctl = replay_ioctl,
+};
+
 
 static void s3c_ts_timer_function (unsigned long data);
 
@@ -197,12 +351,18 @@ static void s3c_ts_timer_function (unsigned long data)
 	}
 }
 
-
 static int __init s3c_ts_init(void)
 {
 	struct clk *clk;
 
 	printk("--- %s ---\r\n",__func__);
+
+	replay_buf = kmalloc(MYLOG_BUF_LEN,GFP_KERNEL);
+	if (!replay_buf){
+		printk("can't alloc for mylog_buf\r\n");
+		return -EIO;
+	}
+	
 
 	// 分配一个input_dev结构体
 	s3c_ts_dev = input_allocate_device();
@@ -260,19 +420,37 @@ static int __init s3c_ts_init(void)
 
 	enter_wait_pen_down_mode();
 
+	// add /dev/input_emu
+	major = register_chrdev(0, "input_replay",&replay_fops);
+	
+	cls = class_create(THIS_MODULE, "input_replay");
+	device_create(cls, NULL, MKDEV(major,0), "input_emu");
+
+	init_timer(&replay_timer);
+	replay_timer.function = input_replay_timer_functions;
+	
+
 	return 0;
 }
 
 static void __exit s3c_ts_exit(void)
 {
 	printk("--- %s ---\r\n",__func__);
+
+	del_timer(&replay_timer);
+
+	device_destroy(cls, MKDEV(major,0));
+	class_destroy(cls);
+	unregister_chrdev(major, "input_replay");
+	
 	del_timer(&ts_timer);
 	free_irq(IRQ_ADC,NULL);
 	free_irq(IRQ_TC,NULL);
 	iounmap(s3c_ts_regs);
 	input_unregister_device(s3c_ts_dev);
 	input_free_device(s3c_ts_dev);
-	
+
+	kfree(replay_buf);	
 }
 
 module_init(s3c_ts_init);
