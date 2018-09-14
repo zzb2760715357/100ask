520299955351016134b803a72c68b2288970ef31
zhuangzb <2760715357@qq.com>
2018-09-14 10:32:11 +0800

    add i2c adapter detect

---
+++ driver_and_test/28-linux-i2c/4th_all_adapter_detect/at24cxx_dev.c
@@ -0,0 +1,52 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/i2c.h>
+#include <linux/err.h>
+#include <linux/regmap.h>
+#include <linux/mfd/88pm860x.h>
+#include <linux/slab.h>
+
+static const unsigned short addr_list[] = {
+		0x60,0x50,
+		I2C_CLIENT_END
+	};
+
+static struct i2c_client *at24cxx_client;
+
+static int at24cxx_dev_init(void)
+{
+	struct i2c_adapter * i2c_adap;
+	struct i2c_board_info at24cxx_info;
+
+	printk("--- %s ---\r\n",__func__);
+	
+	memset(&at24cxx_info, 0, sizeof(struct i2c_board_info));
+	strlcpy(at24cxx_info.type, "at24c08", I2C_NAME_SIZE);
+	
+	i2c_adap = i2c_get_adapter(0);
+	at24cxx_client = i2c_new_probed_device(i2c_adap, &at24cxx_info, addr_list, NULL);
+	i2c_put_adapter(i2c_adap);
+
+	if (at24cxx_client)
+		return 0;
+	else
+		return -ENODEV;
+}
+
+static void at24cxx_dev_exit(void)
+{	
+	printk("--- %s ---\r\n",__func__);
+
+	i2c_unregister_device(at24cxx_client);
+}
+
+
+module_init(at24cxx_dev_init);
+module_exit(at24cxx_dev_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("zhuangzebin");
+MODULE_DESCRIPTION("at24cxx device");
+
+
