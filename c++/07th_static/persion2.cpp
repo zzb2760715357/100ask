fd3aeac2025e7b50e2e6e1201b75dafb3fa10fe2
zhuangzebin <2760715357@qq.com>
2018-10-16 10:34:36 +0800

    add static friend demo

---
+++ c++/07th_static/persion2.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+
+using namespace std;
+
+class person {
+private:
+	char *name;
+	int age;
+	char *work;
+	static int cnt;
+	
+public:
+
+	static int getCount(void);
+
+	person()
+	{
+		//cout << "person()" << endl;
+		name = NULL;
+		work = NULL;
+		cnt ++;
+	}
+	person(char *name)
+	{
+		//cout << "person(char *name)" << endl;
+		this->name = new char[strlen(name)+1];
+		strcpy(this->name,name);
+
+		this->work = NULL;
+		cnt ++;
+	}
+	person(char *name,int age,char *work = "none")
+	{
+		cout << "person(char *name,int age),name = " << name << ",age = "<< age << endl;
+		this->age = age;
+		
+		this->name = new char[strlen(name)+1];
+		strcpy(this->name,name);
+		
+		this->work = new char[strlen(work)+1];
+		strcpy(this->work,work);
+		cnt ++;
+	}
+
+	person(person &per)
+	{
+		cout << "person(person &per)" << endl;
+		this->age = per.age;
+
+		this->name = new char[strlen(per.name)+1];
+		strcpy(this->name,per.name);
+		
+		this->work = new char[strlen(per.work)+1];
+		strcpy(this->work,per.work);
+		cnt ++;
+	}
+	
+	~person()
+	{
+		cout << "~person()" << endl;
+	
+		if (this->name){
+			cout << "name = " << name << endl;
+			delete this->name;
+		}
+		if (this->work){
+			cout << "work = " << work << endl;
+			delete this->work;
+		}
+	}
+	
+	void setName(char *name)
+	{
+		this->name = name;
+	}
+
+	int setAge(int age)
+	{
+		if (age < 0 || age > 150){
+			this->age = 0;
+			return -1;
+		}
+		this->age = age;
+		return 0;
+	}
+	void setWork(char *work)
+	{
+		this->work = work;
+	}
+	void printInfo(void)
+	{
+		//printf("name = %s, age = %d, work = %s\n", name, age, work); 
+		cout << "name = " << name << ",age = " << age <<",work = " << work << endl;
+	}
+};
+
+int person::cnt = 0;
+int person::getCount(void)
+{
+	return cnt;
+}
+
+int main(int argc,char **argv)
+{
+	person p[100];
+	
+
+	cout << "person number = " << person::getCount()<<endl;
+	cout << "person number = " << p[0].getCount()<<endl;	
+	cout << "person number = " << p[1].getCount()<<endl;
+
+	return 0;
+}
+
+
+
