ea7dcc808c52058f34946387634117470a97ae3f
zhuangzebin <2760715357@qq.com>
2019-07-07 17:52:31 +0800

    C调java非静态方法

---
+++ java/13_jni/07/Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+	}
+
+	public int sayhello_to(String name){
+		System.out.println("java Hello World! " + name);
+		return 123;
+	}
+
+	public static void sayhello_to(){
+	
+	}
+	
+}
